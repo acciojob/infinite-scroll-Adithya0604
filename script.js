7553df1e2a78963dc7a82ee352c051e857caf06c
S.Adithya Sharma <120710617+Adithya0604@users.noreply.github.com>
2025-06-04 16:27:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
-//your code here!
+const loadMore = document.getElementById('infi-list');
+const Box = document.querySelector('.infi-list-class');
 
+function ScrollMore(loading = 10){
+	for (let i = 0; i < loading; i++) {
+	const LI = document.createElement('li');
+	LI.classList.add('infi-list-li-class');
+	LI.textContent = `Item ${Box.children.length}`
+	Box.appendChild(LI);
+	}
+}
+
+ScrollMore();
+
+Box.addEventListener('scroll', () => {
+	if (Box.scrollTop + Box.clientHeight >= Box.scrollHeight - 10){
+		ScrollMore(2);
+	}
+});
